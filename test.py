e5411d8237625edda9be1dee2e5965971cfe2f97
saminal1 <72281134+saminal1@users.noreply.github.com>
2022-08-13 21:28:25 +1000

    a20 update

--- test.py
+++ test.py
@@ -3,10 +3,21 @@ from pathlib import Path
 import glob
 import re
 import xml.etree.ElementTree as ET
+import xlsxwriter
+
+# set up xlsx file
+
+workbook = xlsxwriter.Workbook('mapvalreport.xlsx')
+worksheet = workbook.add_worksheet()
+tableheading = workbook.add_format({'bold': True, 'rotation': 45})
+tablegrade = workbook.add_format({'align': 'center'})
+poifail = workbook.add_format({'font_color': 'red'})
+mapfail = workbook.add_format({'bold': True, 'font_color': 'red'})
+mappass = workbook.add_format({'bold': True, 'font_color': 'green'})
 
 # define class keypoi to store requirements
 
-class keypoi:
+class keypoi(object):
     def __init__(self, filename, friendlyname, required, detected):
         self.filename = filename
         self.friendlyname = friendlyname
@@ -34,6 +45,25 @@ reqlist.append(keypoi("traderne", "NE Traders", 2, 0))
 reqlist.append(keypoi("tradersw", "SW Traders", 2, 0))
 reqlist.append(keypoi("traderse", "SE Traders", 2, 0))
 
+# add column headers in first line
+
+row = 0
+column = 0
+worksheet.set_row(row, None, tableheading)
+worksheet.write(row, column, 'World')
+column += 1
+
+for req in reqlist:
+    worksheet.write(row, column, req.friendlyname)
+    worksheet.set_column(column, column, 6)
+    column += 1
+
+worksheet.write(row, column, 'Score')
+worksheet.set_column(column, column, 6)
+column += 1
+worksheet.write(row,column, 'Grade')
+worksheet.set_column(column, column, 6, tablegrade)
+
 # read in prefabs.xml files - recurse through world directories in a root path
 glob_path = Path(r"J:\Games\7D2D\TestWorlds")
 file_list = [str(pp) for pp in glob_path.glob("**/prefabs.xml")]
@@ -42,20 +72,35 @@ file_list = [str(pp) for pp in glob_path.glob("**/prefabs.xml")]
 
 passes = []
 
+# set longest world name to 0
+
+maxworld = 0
 # start checking xml files
 
 for file in file_list:
+
+    # start new excel line
+
+    row += 1
+    column = 0
 
     # get world name from path name
     world = re.search("([A-Za-z0-9\-\s]*)(?=.prefabs\.xml$)", file).group()
     print(world)
     print("")
 
+    worksheet.write(row, column, world)
+
+    # increase longest world name if this world is longer than the previous longest
+
+    if len(world) > maxworld:
+        maxworld = len(world)
+        
     # reset detected counters
     for req in reqlist:
         req.detected = 0
 
-    #read in xml
+    # read in xml
     prefabs = ET.parse(file)
     root = prefabs.getroot()
 
@@ -68,14 +113,14 @@ for file in file_list:
             zpos = int(re.search("(-?\d{1,4}$)", position).group())
             if zpos >= 0:
                 if xpos < 0:
-                    keypoi.found(reqlist[-4]) # Found in NW quadrant
+                    keypoi.found(reqlist[-4]) # found in NW quadrant
                 else:
-                    keypoi.found(reqlist[-3]) # Found in NE quadrant
+                    keypoi.found(reqlist[-3]) # found in NE quadrant
             else:
                 if xpos < 0:
-                    keypoi.found(reqlist[-2]) # Found in SW quadrant
+                    keypoi.found(reqlist[-2]) # found in SW quadrant
                 else:
-                    keypoi.found(reqlist[-1]) # Found in SE quadrant
+                    keypoi.found(reqlist[-1]) # found in SE quadrant
         else:
             for req in reqlist[:-4]:
                 if re.match(req.filename, name):
@@ -87,19 +132,37 @@ for file in file_list:
 
     # calculate score
     for req in reqlist:
+        
+        column += 1
+        
         if int(req.detected) >= int(req.required):
             print(f"{req.friendlyname}: {req.detected} of {req.required} found - Pass")
             score += 1
+
+            worksheet.write(row,column,req.detected)
         else:
             print(f"{req.friendlyname}: {req.detected} of {req.required} found - Fail")
+            
+            worksheet.write(row,column,req.detected,poifail)
+
     print("")
-    # calculate pass and add to the list if appropriate
-    if score == len(reqlist):
-        print(f"{world} score out of {len(reqlist)}: {score} - Pass")
-        passes.append(world)
+    column += 1
+
+    # print score and calculate grade
+
+    grades = ["A", "B", "C", "D", "E", "F"]
+    if len(reqlist) - score >= 5:
+        grade = grades[5]
     else:
-        print(f"{world} score out of {len(reqlist)}: {score} - Fail")
-    print("\n")
+        grade = grades[len(reqlist) - score]
+        if grade == "A":
+            passes.append(world)
+    
+    print(f"{world} score out of {len(reqlist)}: {score} - Grade {grade}")
+    worksheet.write(row,column,score)
+    column += 1
+    worksheet.write(row,column,grade)
+
 
 # print summary
 if len(passes) == 0:
@@ -108,3 +171,8 @@ else:
     print("List of passes:")
     for apass in passes:
         print(apass)
+
+# set world name column width to (longest world name + 2 characters) for spacing
+maxworld += 2
+worksheet.set_column(0,0,maxworld)
+workbook.close()
